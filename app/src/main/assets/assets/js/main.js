41c815e4bcfde4300dcd152f2499f233309fd930
Huntmix <73614708+huntmix@users.noreply.github.com>
2021-01-20 19:38:37 +0200

    8

---
+++ app/src/main/assets/assets/js/main.js
@@ -0,0 +1,561 @@
+//WPS FUNCTION
+function ShowWps() {
+  var x = document.getElementById("wps");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+  var y = document.getElementById("wps2");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+	var y = document.getElementById("wps3");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+	
+  }
+  var change2 = document.getElementById("showwps2");
+                if (change2.innerHTML === "Скрыть")
+                {
+                    change2.innerHTML = "Не  вышло!";
+                }
+}
+var y = document.getElementById("notice");
+y.style.display = "none";
+ }
+ //wps2
+function ShowWps2() {
+  var x = document.getElementById("wps2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+  var x = document.getElementById("wps3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  }
+  var change = document.getElementById("showwps2");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Не  вышло!";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+
+}
+//wps3
+function ShowWps3() {
+  var x = document.getElementById("wps3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+  var change = document.getElementById("showwps3");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Не  вышло!";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+				
+}
+
+//NETHUNTER FUNCTION
+function ShowNh() {
+  var x = document.getElementById("nh");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+  var y = document.getElementById("nh2");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+
+  var change2 = document.getElementById("shownh2");
+                if (change2.innerHTML === "Скрыть")
+                {
+                    change2.innerHTML = "Не  вышло!";
+                }
+}
+var y = document.getElementById("notice");
+y.style.display = "none";
+ }
+ //nethunter2
+function ShowNh2() {
+  var x = document.getElementById("nh2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+
+  var change = document.getElementById("shownh2");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Не  вышло!";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+//NO ROOT
+function ShowNoRoot() {
+  var x = document.getElementById("NoRoot");
+  var y = document.getElementById("notice");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+}
+y.style.display = "none";
+
+}
+
+//IMAGES 
+
+function ImageWAP() {
+  var x = document.getElementById("WAP");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("IWAP");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+//Wifi warden
+function ImageWW() {
+  var x = document.getElementById("WW");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("IWW");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+
+//WPS WPA TESTER
+function ImageWWT() {
+  var x = document.getElementById("WWT");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("IWWT");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+//WIBR+
+function ImageWIBR() {
+  var x = document.getElementById("WIBR");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("IWIBR");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+
+// MSF
+function ImageMSF() {
+  var x = document.getElementById("msf5");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("imsf");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+//Donation
+function ShowD() {
+  var x = document.getElementById("showg");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+}}
+
+function ShowList() {
+  var x = document.getElementById("list");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+}}
+
+//nethunter function
+function NHone() {
+  var x = document.getElementById("install1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+  var y = document.getElementById("install2");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+	
+  }
+  var y = document.getElementById("install3");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+	
+  }
+  var change = document.getElementById("showm1");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Способ №1";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+  var change2 = document.getElementById("showm2");
+                if (change2.innerHTML === "Скрыть")
+                {
+                    change2.innerHTML = "Способ №2";
+                }
+  var change2 = document.getElementById("showm3");
+                if (change2.innerHTML === "Скрыть")
+                {
+                    change2.innerHTML = "Способ №3 (Рек.)";
+                }				
+}
+
+
+function NHtwo() {
+  var x = document.getElementById("install2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+  var y = document.getElementById("install1");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+	
+  }
+  var y = document.getElementById("install3");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+	
+  }
+  var change = document.getElementById("showm2");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Способ №2";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+  var change2 = document.getElementById("showm1");
+                if (change2.innerHTML === "Скрыть")
+                {
+                    change2.innerHTML = "Способ №1";
+                }
+  var change2 = document.getElementById("showm3");
+                if (change2.innerHTML === "Скрыть")
+                {
+                    change2.innerHTML = "Способ №3 (Рек.)";
+                }		
+}
+function NHthird() {
+  var x = document.getElementById("install3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+  var y = document.getElementById("install1");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+	
+  }
+  var y = document.getElementById("install2");
+  if (y.style.display === "block") {
+    y.style.display = "none";
+	
+  }
+  var change = document.getElementById("showm3");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Способ №3 (Рек.)";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+  var change2 = document.getElementById("showm1");
+                if (change2.innerHTML === "Скрыть")
+                {
+                    change2.innerHTML = "Способ №1";
+                }
+  var change2 = document.getElementById("showm2");
+                if (change2.innerHTML === "Скрыть")
+                {
+                    change2.innerHTML = "Способ №2";
+                }			
+				
+}
+
+//FISHER
+function Fishka() {
+  var x = document.getElementById("fisher");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("fishb");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function Fishka2() {
+  var x = document.getElementById("fisher2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("fishb2");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function ArsenalImg() {
+  var x = document.getElementById("arsenalpic");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("arsenal");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function CmdImg() {
+  var x = document.getElementById("cmdpic");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("cmd");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function HidImg() {
+  var x = document.getElementById("hidpic");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("hid");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function HIDshow() {
+  var x = document.getElementById("hidall");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("showhid");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "HID раздел";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function WIREshow() {
+  var x = document.getElementById("wireless");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("wirelessb");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Wireless раздел";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function ManaImg() {
+  var x = document.getElementById("manapic");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("mana");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function DeauthImg() {
+  var x = document.getElementById("deauthpic");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("deauth");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function MSFImg() {
+  var x = document.getElementById("msfpic");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("msf");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
+function NmaperImg() {
+  var x = document.getElementById("nmappic");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+
+  } else {
+    x.style.display = "block";
+	;
+  }
+ var change = document.getElementById("nmaper");
+                if (change.innerHTML == "Скрыть")
+                {
+                    change.innerHTML = "Показать изображение";
+                }
+                else {
+                    change.innerHTML = "Скрыть";
+                }
+}
